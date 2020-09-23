c491d548a919b30432377a260b9a0d050de0aca5
Thomas Kälin <thomas.kaelin1@ost.ch>
2020-09-23 06:58:53 +0200

    WPF - U08: Lösung ergänzt

---
+++ U08_Loesung/Aufgabe 5/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Aufgabe_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SayHelloButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Greeting.Text = $"Hello, {NameInput.Text}!";
+            NameInput.Text = string.Empty;
+        }
+    }
+}
